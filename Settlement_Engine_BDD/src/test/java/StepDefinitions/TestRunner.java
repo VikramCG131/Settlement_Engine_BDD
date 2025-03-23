60adbcc3a9926b02c3f7b92a6994be727ccd455e
shanofarBagwan <shanofar-aliakaber.bagwan@capgemini.com>
2025-03-23 17:17:32 +0000

    updated testrunner

--- Settlement_Engine_BDD/src/test/java/StepDefinitions/TestRunner.java
+++ Settlement_Engine_BDD/src/test/java/StepDefinitions/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/resources/Features",
-    glue = "StepDefinitions",
-    plugin = {"pretty", "html:target/cucumber-reports"}
+    glue = "StepDefinitions",monochrome = true,
+    		plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"}
 )
 public class TestRunner {}
