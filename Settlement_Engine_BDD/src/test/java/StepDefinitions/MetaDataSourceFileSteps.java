ec360f90f187eba0e2a48d0e6bca464e4b4f0d9c
shanofarBagwan <shanofar-aliakaber.bagwan@capgemini.com>
2025-03-24 09:31:44 +0000

    updated code for metadata data source file

--- Settlement_Engine_BDD/src/test/java/StepDefinitions/MetaDataSourceFileSteps.java
+++ Settlement_Engine_BDD/src/test/java/StepDefinitions/MetaDataSourceFileSteps.java
@@ -5,8 +5,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 import Utilities.CommonUtilities;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
@@ -16,104 +17,106 @@ import io.cucumber.java.en.Then;
 public class MetaDataSourceFileSteps {
     WebDriver driver;
     CommonUtilities commonUtilities = new CommonUtilities();
+    WebDriverWait wait;
 
-@Given("The Settlement Engine<URL>")
-public void the_settlement_engine_url() {
-	
-driver=	WebDriverManager.chromedriver().create();
-    driver.manage().window().maximize();
-driver.get("http://localhost:4200");	
-}
-
-
-@When("user hit the Settlement Engine URL")
-	public void the_user_hits_the_settlement_engine_url() throws InterruptedException {
-        Assert.assertTrue(driver.getCurrentUrl().contains("http://localhost:4200"));
-        Thread.sleep(3000);
-        System.out.println("The user is able to login with Settlent Engine URL");
-      
-}
-
-@Then("the user is able to navigate to the Settlement Engine homepage")
-public void the_user_is_able_to_navigate_to_the_settlement_engine_homepage() throws InterruptedException {
-	Assert.assertTrue(driver.getTitle().contains("Starter Kit PF Angular"));
-    Thread.sleep(3000);
-    System.out.println("The user is able to navigate to the Settlement Engine homepage");
-    commonUtilities.screenshot();
-}
-
-@Then("the user click on Data Source File")
-public void the_user_click_on_data_source_file() throws InterruptedException {
-
-    driver.findElement(By.xpath("//a[contains(text(),'Data Source File')]")).click();
-    Thread.sleep(8000);
-    System.out.println("The user is able to navigate to Data Source File page");
-    commonUtilities.screenshot();
-    
-}
-
-@Then("the user click on Add New Button")
-public void the_user_click_on(String string) throws InterruptedException {
-	Actions actions = new Actions(driver);
-	WebElement button = driver.findElement(By.xpath("//button[contains(text(),'Add New')]"));
-	actions.moveToElement(button).click().perform();
-	Thread.sleep(3000);
-	System.out.println("The user is able to click on Add New Button");
-	  commonUtilities.screenshot();
-	}
-
-@Then("the user enter {string} in the {string} field")
-public void the_user_enter_in_the_field(String string, String string2) {
-    driver.findElement(By.xpath("//input[@formcontrolname='fileName']")).sendKeys("Testing");
-    driver.findElement(By.xpath("//input[@formcontrolname='filePattern']")).sendKeys("Pattern");
-    driver.findElement(By.xpath("//input[@formcontrolname='filePathRaw']")).sendKeys("Raw");
-    driver.findElement(By.xpath("//input[@formcontrolname='filePathCleansed']")).sendKeys("Cleansed");
-    driver.findElement(By.xpath("//input[@formcontrolname='fileSizeMB']")).sendKeys("10");
-    driver.findElement(By.xpath("//input[@formcontrolname='fileType']")).sendKeys("Type");
-    driver.findElement(By.xpath("//input[@formcontrolname='stagingTableName']")).sendKeys("MetadataSourceFile");
-    driver.findElement(By.xpath("//input[@formcontrolname='fileDelimiter']")).sendKeys("Delimiter");
-    driver.findElement(By.xpath("//input[@formcontrolname='headerIdentifier']")).sendKeys("Header");
-    driver.findElement(By.xpath("//input[@formcontrolname='columnIdentifier']")).sendKeys("Column");
-}
-
-@Then("the user should see {string} in the file list")
-public void the_user_should_see_in_the_file_list(String string) throws InterruptedException {
-    driver.findElement(By.xpath("//input[@formcontrolname='fileType']")).sendKeys("Test");
-    System.out.println("The user is able to insert the data in the all fields");
-    Thread.sleep(2000);
-    commonUtilities.screenshot();
-}
-
-@Then("the user should see click on the Submit Button")
-public void the_user_should_click_on_the_Submit_button(String string) {
-    driver.findElement(By.xpath("//button[contains(text(),'Submit')]")).click();
-    System.out.println("The user is able to click on the Submit Button");
-    driver.quit();
-}
-
-@Then("the user should see validation errors for required fields")
-public void the_user_should_see_validation_errors_for_required_fields() {
- driver.findElement(By.xpath("//input[@formcontrolname='fileName']")).sendKeys("Testing");
-	driver.findElement(By.xpath("//input[@formcontrolname='filePattern']")).sendKeys("Pattern");
-	driver.findElement(By.xpath("//input[@formcontrolname='filePathCleansed']")).sendKeys("Cleansed");
-	driver.findElement(By.xpath("//input[@formcontrolname='fileSizeMB']")).sendKeys("10");
-	driver.findElement(By.xpath("//input[@formcontrolname='fileType']")).sendKeys("Type");
-	driver.findElement(By.xpath("//input[@formcontrolname='fileDelimiter']")).sendKeys("Delimiter");
-	driver.findElement(By.xpath("//input[@formcontrolname='headerIdentifier']")).sendKeys("Header");
-	driver.findElement(By.xpath("//input[@formcontrolname='columnIdentifier']")).sendKeys("Column");
-	driver.findElement(By.xpath("//button[contains(text(),'Submit')]")).click();   
-	  commonUtilities.screenshot();
-}
-
-@Then("{string} should not be in the file list")
-public void should_not_be_in_the_file_list(String string) {
-    driver.findElement(By.xpath("//div[@data-ref='rootWrapperBody']")).isDisplayed();	
-    driver.quit();
+    @Given("The Settlement Engine<URL>")
+    public void the_settlement_engine_url() {
+        driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
+        driver.get("http://localhost:4200");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-@Then("the user click on {string}")
-public void the_user_click_on_Cancel(String string) {
-  //  driver.findElement(By.xpath("//button[contains(text(),'Cancel')]")).click();
-}
+    @When("user hit the Settlement Engine URL")
+    public void the_user_hits_the_settlement_engine_url() throws InterruptedException {
+        Assert.assertTrue(driver.getCurrentUrl().contains("http://localhost:4200"));
+        Thread.sleep(3000);
+        System.out.println("The user is able to login with Settlement Engine URL");
+    }
 
+    @Then("the user is able to navigate to the Settlement Engine homepage")
+    public void the_user_is_able_to_navigate_to_the_settlement_engine_homepage() throws InterruptedException {
+        Assert.assertTrue(driver.getTitle().contains("Starter Kit PF Angular"));
+        Thread.sleep(3000);
+        System.out.println("The user is able to navigate to the Settlement Engine homepage");
+        commonUtilities.screenshot();
+    }
+
+    @Then("the user click on Data Source File")
+    public void the_user_click_on_data_source_file() throws InterruptedException {
+        driver.findElement(By.xpath("//a[contains(text(),'Data Source File')]")).click();
+        Thread.sleep(8000);
+        System.out.println("The user is able to navigate to Data Source File page");
+        commonUtilities.screenshot();
+    }
+    @When("the user clicks the Add New button")
+    public void the_user_clicks_the_add_new_button() {
+    	WebElement button = driver.findElement(By.xpath("//*[text()='Add New']"));
+        wait.until(ExpectedConditions.visibilityOf(button)).click(); 
+        System.out.println("The user is able to click on Add New Button");
+        commonUtilities.screenshot();
+    }
+
+
+    @Then("the user enters the data in required fields")
+    public void the_user_enters_the_data_in_required_fields() {
+    	int random =(int) (Math.floor(Math.random()*100000) + 1);
+        driver.findElement(By.xpath("//input[@formcontrolname='fileName']")).sendKeys("TestAutomation"+random);
+        driver.findElement(By.xpath("//input[@formcontrolname='fileType']")).sendKeys("csv");
+        driver.findElement(By.xpath("//input[@formcontrolname='filePattern']")).sendKeys("raw");
+        driver.findElement(By.xpath("//input[@formcontrolname='filePathRaw']")).sendKeys("C://local");
+        driver.findElement(By.xpath("//input[@formcontrolname='filePathCleansed']")).sendKeys("D://local");
+        driver.findElement(By.xpath("//input[@formcontrolname='fileSizeMB']")).sendKeys("10");
+        //driver.findElement(By.xpath("//input[@formcontrolname='fileType']")).sendKeys("Type");
+        driver.findElement(By.xpath("//input[@formcontrolname='stagingTableName']")).sendKeys("MetadataSourceFile");
+        driver.findElement(By.xpath("//input[@formcontrolname='fileDelimiter']")).sendKeys(",");
+        driver.findElement(By.xpath("//input[@formcontrolname='headerIdentifier']")).sendKeys(",");
+        driver.findElement(By.xpath("//input[@formcontrolname='columnIdentifier']")).sendKeys(",");
+    }
+
+    @When("the user should see click on the Submit Button")
+    public void the_user_should_click_on_the_Submit_Button() {
+        driver.findElement(By.xpath("//button[contains(text(),'Submit')]")).click();
+        System.out.println("The user is able to click on the Submit Button");
+   	 	commonUtilities.screenshot();
+    }
+    @Then("the user should see {string} in the file list")
+    public void the_user_should_see_in_the_file_list(String string) throws InterruptedException {
+       String actual= driver.findElement(By.xpath("//*[text()=' Record has been added successFully ']")).getText();
+       Assert.assertEquals("Record has been added successFully", actual);
+        System.out.println("The user is able to insert the data in the all fields");
+        Thread.sleep(2000);
+        commonUtilities.screenshot();
+    }
+
+    @Then("the user should see validation errors for required fields")
+    public void the_user_should_see_validation_errors_for_required_fields() {
+        driver.findElement(By.xpath("//button[contains(text(),'Submit')]")).click();
+        commonUtilities.screenshot();
+    }
+
+    @Then("{string} should not be in the file list")
+    public void should_not_be_in_the_file_list(String string) {
+        driver.findElement(By.xpath("//div[@data-ref='rootWrapperBody']")).isDisplayed();
+        driver.quit();
+    }
+
+    @Then("the user click on {string}")
+    public void the_user_click_on_button(String string) {
+        driver.findElement(By.xpath("//button[contains(text(),'Reset')]")).click();
+        System.out.println("The fields are empty");
+        commonUtilities.screenshot();
+    }
+
+    @When("the user clicks the {string} button")
+    public void the_user_clicks_the_button(String string) {
+        driver.findElement(By.xpath("//button[contains(text(),'Cancel')]")).click();
+        System.out.println("The user is able to close the Add New page");
+        commonUtilities.screenshot();
+    }
+
+    @When("the user clicks the {string} buttons")
+    public void the_user_clicks_the_buttons(String string) {
+        // Implementation for this step
+    }
 }
--- Settlement_Engine_BDD/src/test/resources/Features/metadata_Data_Source_File.feature
+++ Settlement_Engine_BDD/src/test/resources/Features/metadata_Data_Source_File.feature
@@ -8,29 +8,21 @@ Feature: Meta Data Form Submission
      And the user click on Data Source File 
      
 
-  Scenario: Verify Meta Data form submission with valid data
-    When the user clicks the "Add New" button
-    And the user enter "test_file" in the "File Name" field
-    And the user enter "CSV" in the "File Type" field
-    And the user enter "/raw/path/test_file.csv" in the "File path raw" field
-    And the user enter "/cleansed/path/test_file.csv" in the "File Path Cleansed" field
-    And the user enter "test_pattern" in the "File Pattern" field
-    And the user enter "10" in the "File Max Sized(MB)" field
-    And the user enter "comma" in the "File Delimeter" field
-    And the user enter "header1" in the "Header Identifier" field
-    And the user click on "Submit"
+ Scenario: Verify Meta Data form submission with valid data
+    When the user clicks the Add New button
+    And the user enters the data in required fields
+    And the user should see click on the Submit Button
     Then the user should see "test_file" in the file list
 
-  Scenario: Verify validation errors when mandatory fields are missing
-    When the user clicks the "Add New" button
-    And the user click on "Submit"
-    Then the user should see validation errors for required fields
+#  Scenario: Verify validation errors when mandatory fields are missing
+#    When the user clicks the Add New button
+#    And the user click on "Submit"
+#    Then the user should see validation errors for required fields
 
-  Scenario: Verify that clicking "Cancel" does not save data
-      When the user clicks the "Add New" buttons
-    And the user enter "test_cancel" in the "File Name" field
-    And the user click on "Cancel"
-    Then "test_cancel" should not be in the file list
+#  Scenario: Verify that clicking "Cancel" does not save data
+#    When the user clicks the "Add New" buttons
+#    And the user click on "Cancel"
+#   Then "test_cancel" should not be in the file list
     
   
   
