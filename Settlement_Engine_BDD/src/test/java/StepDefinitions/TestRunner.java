e6b54c3a2e1e2c4bebc70d282b945523fc082873
VikramCG131 <vikramasimhareddy.sangevareddy@capgemini.com>
2025-04-17 10:25:00 +0000

    updated

--- Settlement_Engine_BDD/src/test/java/StepDefinitions/TestRunner.java
+++ Settlement_Engine_BDD/src/test/java/StepDefinitions/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = 
-    		"src/test/resources/Features/Reference_LookUp/client_LookUp.feature" //the path of the feature files
+    		"src/test/resources/Features/metadata/b_metadata_Column.feature" //the path of the feature files
     , //the path of the feature files			  
     		
     glue = "StepDefinitions",
