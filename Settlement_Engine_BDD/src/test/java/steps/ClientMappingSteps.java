9ba52d08a04bdcd0a7c3255ef27be5cc5c991dac
shanofarbagwan <shanofar-aliakaber.bagwan@capgemini.com>
2025-04-23 11:52:49 +0530

    updated client mapping

---
+++ Settlement_Engine_BDD/src/test/java/steps/ClientMappingSteps.java
@@ -0,0 +1,186 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ClientMappingSteps {
+
+	@Given("Settlement_Engine<URL>")
+	public void settlement_engine_url() {
+	    
+	}
+
+	@When("the user clicks Settlement Engine URL for the Client Mapping")
+	public void the_user_clicks_settlement_engine_url_for_the_client_mapping() {
+	   
+	}
+
+	@When("the User clicks on Reference LookUp UI screen for the Client Mapping")
+	public void the_user_clicks_on_reference_look_up_ui_screen_for_the_client_mapping() {
+	   
+	}
+
+	@Then("the User is able to navigate to the Settlement Engine Homepage for the Client Mapping")
+	public void the_user_is_able_to_navigate_to_the_settlement_engine_homepage_for_the_client_mapping() {
+	   
+	}
+
+	@Then("User Clicks on Hamburger Button Icon for the Client Mapping")
+	public void user_clicks_on_hamburger_button_icon_for_the_client_mapping() {
+	
+	}
+
+	@When("the user click on the Client Mapping")
+	public void the_user_click_on_the_client_mapping() {
+	   
+	}
+
+	@When("the user clicks the Add button from the Client Mapping page")
+	public void the_user_clicks_the_add_button_from_the_client_mapping_page() {
+	    
+	}
+
+	@When("the user enters the data in required fields for the Client Mapping")
+	public void the_user_enters_the_data_in_required_fields_for_the_client_mapping() {
+	   
+	}
+
+	@When("the user should see click on the Submit Button for the Client Mapping")
+	public void the_user_should_see_click_on_the_submit_button_for_the_client_mapping() {
+	    
+	}
+
+	@Then("the user should see {string} in the file list for the Client Mapping")
+	public void the_user_should_see_in_the_file_list_for_the_client_mapping(String string) {
+	
+	}
+
+	@Then("the user click on any field for the Client Mapping and the user should see validation errors for required fields for the Client Mapping")
+	public void the_user_click_on_any_field_for_the_client_mapping_and_the_user_should_see_validation_errors_for_required_fields_for_the_client_mapping() {
+	   
+	}
+
+	@When("the user click on Reset button for the Client Mapping")
+	public void the_user_click_on_reset_button_for_the_client_mapping() {
+	  
+	}
+
+	@Then("all the fields should be cleared for the Client Mapping")
+	public void all_the_fields_should_be_cleared_for_the_client_mapping() {
+	
+	}
+
+	@When("the user click on Cancel button for the Client Mapping")
+	public void the_user_click_on_cancel_button_for_the_client_mapping() {
+	
+	}
+
+	@Then("Add Pop up should be closed for the Client Mapping")
+	public void add_pop_up_should_be_closed_for_the_client_mapping() {
+	
+	}
+
+	@When("The User click the edit icon for the any Client Mapping")
+	public void the_user_click_the_edit_icon_for_the_any_client_mapping() {
+	  
+	}
+
+	@Then("The User should see the edit modal open Client Mapping")
+	public void the_user_should_see_the_edit_modal_open_client_mapping() {
+	    
+	}
+
+	@Then("the user update the any field for the Client Mapping")
+	public void the_user_update_the_any_field_for_the_client_mapping() {
+	
+	}
+
+	@Then("the user should see click on the Update Button for the Client Mapping")
+	public void the_user_should_see_click_on_the_update_button_for_the_client_mapping() {
+	   
+	}
+
+	@Then("the user should see the updated {string} in the file list for the Client Mapping")
+	public void the_user_should_see_the_updated_in_the_file_list_for_the_client_mapping(String string) {
+	  
+	}
+
+	@When("The User click the delete icon for the any Client Mapping")
+	public void the_user_click_the_delete_icon_for_the_any_client_mapping() {
+	  
+	}
+
+	@Then("The User should see a confirmation pop-up with title {string} for the Client Mapping")
+	public void the_user_should_see_a_confirmation_pop_up_with_title_for_the_client_mapping(String string) {
+
+	}
+
+	@Then("The User should see {string} message for the Client Mapping")
+	public void the_user_should_see_message_for_the_client_mapping(String string) {
+	  
+	}
+
+	@When("The User should see a confirmation pop-up with Cancel and Confirm buttons for the Client Mapping")
+	public void the_user_should_see_a_confirmation_pop_up_with_cancel_and_confirm_buttons_for_the_client_mapping() {
+	
+	}
+
+	@When("The User confirm the deletion for the Client Mapping")
+	public void the_user_confirm_the_deletion_for_the_client_mapping() {
+	 
+	}
+
+	@Then("the file should be removed from the table for the Client Mapping")
+	public void the_file_should_be_removed_from_the_table_for_the_client_mapping() {
+	 
+	}
+
+	@When("user clicks the download button for the Client Mapping page")
+	public void user_clicks_the_download_button_for_the_client_mapping_page() {
+	   
+	}
+
+	@Then("a file should be downloaded to the default download folder for the Client Mapping page")
+	public void a_file_should_be_downloaded_to_the_default_download_folder_for_the_client_mapping_page() {
+	
+	}
+
+	@When("the User Navigates to upload Button from the Client Mapping page")
+	public void the_user_navigates_to_upload_button_from_the_client_mapping_page() {
+	  
+	}
+
+	@When("user selects a valid file to upload for the Client Mapping")
+	public void user_selects_a_valid_file_to_upload_for_the_client_mapping() {
+	   
+	}
+
+	@When("the file should be successfully uploaded for the Client Mapping")
+	public void the_file_should_be_successfully_uploaded_for_the_client_mapping() {
+	   
+	}
+
+	@When("user click the Send for Approval button for the Client Mapping")
+	public void user_click_the_send_for_approval_button_for_the_client_mapping() {
+
+	}
+
+	@Then("the file should be submitted for approval for the Client Mapping")
+	public void the_file_should_be_submitted_for_approval_for_the_client_mapping() {
+
+	}
+
+	@Then("a confirmation message should be displayed with Confirm and Cancel buttons for the Client Mapping")
+	public void a_confirmation_message_should_be_displayed_with_confirm_and_cancel_buttons_for_the_client_mapping() {
+	   
+	}
+
+	@Then("by clicking on Confirm button the file should be sent for approval for the Client Mapping")
+	public void by_clicking_on_confirm_button_the_file_should_be_sent_for_approval_for_the_client_mapping() {
+	 
+	}
+
+
+
+}
