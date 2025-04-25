8b01448005a1d35f92b0dd57efd86888be917657
VikramCG131 <vikramasimhareddy.sangevareddy@capgemini.com>
2025-04-25 11:13:26 +0000

    updated report utility

--- Settlement_Engine_BDD/src/test/java/utils/ReportUtil.java
+++ Settlement_Engine_BDD/src/test/java/utils/ReportUtil.java
@@ -32,13 +32,13 @@ public class ReportUtil {
         extent.setSystemInfo("Run By", "Shilajeet Chakraborty");
     }
  
-    // Create Feature
+     // Create Feature
     public static void createFeature(String featureName) {
         featureTest = extent.createTest(MarkupHelper.createLabel(featureName, ExtentColor.PURPLE).getMarkup());
-        if (extent == null) {
+       /* if (extent == null) {
             throw new IllegalStateException("ExtentReports is not initialized. Call initializeReport() first.");
         }
-        extent.createTest(featureName);
+        extent.createTest(featureName);*/
     }
  
     // Create Scenario as a Child Step
